fea58127fb6bd467c0be349669c10a756e377f31
bartoszkoziel <83342505+bartoszkoziel@users.noreply.github.com>
2023-05-16 14:35:08 +0200

    Add files via upload

--- app/router.js
+++ app/router.js
@@ -1,13 +1,25 @@
-const fileController = require("./fileController")
+const fileController = require("./controller")
+const logger = require("tracer").colorConsole()
 
 const router = (req, res) => {
     if(req.method == "POST" && req.url == "/api/photos"){
         fileController.saveImg(req, res)
-        console.log("ADDED PHOTO AND ALBUM")
+        logger.info("ADDED PHOTO AND ALBUM")
     }
-    else if(req.method == "GET" && req.url == "/api/photos"){}
-    else if(req.method == "GET" && req.url.match(/\/api\/tasks\/([0-9]+)/)){}
-    else if(req.method == "DELETE" && req.url.match(/\/api\/tasks\/([0-9]+)/)){}
+    else if(req.method == "GET" && req.url == "/api/photos"){
+        fileController.getImg(res, -1)
+        logger.info("SENT ALL THE PHOTOS TO CLIENT")
+    }
+    else if(req.method == "GET" && req.url.match(/\/api\/photos\/([0-9]+)/)){
+        let id = req.url.split("/")[3]
+        fileController.getImg(res, id)
+        logger.info("SENT PHOTO", id, "TO CLIENT")
+    }
+    else if(req.method == "DELETE" && req.url.match(/\/api\/tasks\/([0-9]+)/)){
+        let id = req.url.split("/")[3]
+        fileController.delImg(res, id)
+        logger.info("DELETED PHOTO", id)
+    }
     else if(req.method == "PATCH" && req.url == "/api/photos"){}
 }
 
